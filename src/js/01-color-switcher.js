4eaddad9e11690c3948a57224433c364b646fee6
Maksym Kolii <maksymkolii@gmail.com>
2022-09-30 23:54:49 +0200

    commit

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
 
+const startBtn = document.querySelector('button[data-start]')
+const stopBtn = document.querySelector('button[data-stop]')
+let timerId = null;
+
+stopBtn.addEventListener("click", () => {
+
+    startBtn.disabled = false;
+    stopBtn.disabled = true;
+
+    clearInterval(timerId);
+  });
+
+startBtn.addEventListener("click", () => {
+
+    startBtn.disabled = true;
+    stopBtn.disabled = false;
+
+  timerId = setInterval(() => {
+
+    document.body.style.background = getRandomHexColor();
+    
+  }, 1000);
+})
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
