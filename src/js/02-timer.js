3bfe9a46024eccb71eeb985a47c14a2932304e82
Maksym Kolii <maksymkolii@gmail.com>
2022-10-03 21:04:17 +0200

    commit

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -2,10 +2,7 @@ import flatpickr from 'flatpickr';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import 'flatpickr/dist/flatpickr.min.css';
 
-
 let finalTime = 0;
-let intervalId = null;
-
 const refs = {
   input: document.querySelector('#datetime-picker'),
   startBtn: document.querySelector('button[data-start]'),
@@ -15,83 +12,87 @@ const refs = {
   seconds: document.querySelector('span[data-seconds]'),
 };
 
-refs.startBtn.addEventListener('click', onButtonClick);
-refs.startBtn.disabled = true;
-
 const options = {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-
-    if(selectedDates[0] <= Date.now()){
+    if (selectedDates[0] <= Date.now()) {
       Notify.failure('Please choose a date in the future');
       refs.startBtn.disabled = true;
       return;
     }
     refs.startBtn.disabled = false;
-     finalTime=selectedDates[0];
+    finalTime = selectedDates[0];
   },
 };
 
 flatpickr('input#datetime-picker', options);
 
-const timer = {
+class Timer {
+  constructor({ onTicTac }) {
+    this.intervalId = null;
+    this.onTicTac = onTicTac;
+  }
 
-  start(){
-     intervalId =setInterval(()=>{
+  start() {
+    refs.startBtn.disabled = true;
+    refs.input.disabled = true;
+    this.intervalId = setInterval(() => {
       const realTime = Date.now();
-      const diff = finalTime - realTime
-      const timeComponents =  convertMs(diff)
+      const diff = finalTime - realTime;
+      const timeComponents = this.convertMs(diff);
 
-      if(diff <1000){
-        clearInterval(intervalId)
+      if (diff < 1000) {
+        clearInterval(this.intervalId);
       }
-     updateInterfaceTime(timeComponents)
-      
-     }, 1000)
+      this.onTicTac(timeComponents);
+    }, 1000);
+  }
 
+  convertMs(ms) {
+    // Number of milliseconds per unit of time
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
 
+    // Remaining days
+    const days = this.addLeadingZero(Math.floor(ms / day));
+    // Remaining hours
+    const hours = this.addLeadingZero(Math.floor((ms % day) / hour));
+    // Remaining minutes
+    const minutes = this.addLeadingZero(
+      Math.floor(((ms % day) % hour) / minute)
+    );
+    // Remaining seconds
+    const seconds = this.addLeadingZero(
+      Math.floor((((ms % day) % hour) % minute) / second)
+    );
+
+    return { days, hours, minutes, seconds };
+  }
+
+  addLeadingZero(value) {
+    return String(value).padStart(2, '0');
   }
 }
 
-function onButtonClick(){
-  timer.start();
-  refs.startBtn.disabled = true;
-  refs.input.disabled = true;
-}
+const timer = new Timer({
+  onTicTac: updateInterfaceTime,
+});
+
 
 function updateInterfaceTime({ days, hours, minutes, seconds }) {
-
   refs.days.textContent = `${days}`;
   refs.hourss.textContent = `${hours}`;
   refs.minutes.textContent = `${minutes}`;
   refs.seconds.textContent = `${seconds}`;
-
 }
 
-function addLeadingZero(value) {
-  return String(value).padStart(2, '0');
-}
 
-function convertMs(ms) {
-  // Number of milliseconds per unit of time
-  const second = 1000;
-  const minute = second * 60;
-  const hour = minute * 60;
-  const day = hour * 24;
+refs.startBtn.addEventListener('click', timer.start.bind(timer));
 
-  // Remaining days
-  const days = addLeadingZero(Math.floor(ms / day));
-  // Remaining hours
-  const hours = addLeadingZero(Math.floor((ms % day) / hour));
-  // Remaining minutes
-  const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
-  // Remaining seconds
-  const seconds = addLeadingZero(
-    Math.floor((((ms % day) % hour) % minute) / second)
-  );
 
-  return { days, hours, minutes, seconds };
-}
+
