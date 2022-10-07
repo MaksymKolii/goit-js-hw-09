735d036da8179e4408aec98e7ff8913d60ac70bf
Maksym Kolii <maksymkolii@gmail.com>
2022-10-07 14:58:21 +0200

    commit

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,42 @@
+const firstDelay = 1000;
+const ammount = 6;
+const step = 3000;
+
+/* Напиши скрипт, который при сабмите формы вызывает функцию createPromise(position, delay) столько раз, сколько ввели в поле amount.
+ При каждом вызове передай ей номер создаваемого промиса (position) и задержку учитывая введенную пользователем первую 
+ задержку (delay) и шаг (step).*/
+let position = 1;
+let delay = firstDelay;
+
+for (let i = 0; i < ammount; i += 1) {
+  createPromise(position, delay)
+  .then(({ position, delay }) => {
+    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+  });
+  delay += step;
+  position += 1;
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({position, delay});
+      } else {
+        reject({position, delay});
+      }
+    }, delay);
+  });
 }
+
+// createPromise(2, 500)
+//   .then(({ position, delay }) => {
+//     console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+//   })
+//   .catch(({ position, delay }) => {
+//     console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+//   });
